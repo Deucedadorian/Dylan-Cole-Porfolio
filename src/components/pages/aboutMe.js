4e50fc2d2dac381b4499f370d90ff67c0fc1fc61
Dylan <logboarddjc@gmail.com>
2021-09-13 20:57:41 -0400

    change About Me section to my Brand Statement

--- src/components/pages/aboutMe.js
+++ src/components/pages/aboutMe.js
@@ -6,9 +6,15 @@ export default function AboutMe() {
             <section id="about-me" className="default-tag-structure">
                 <h1>About Me</h1>
                 <p className="article-text">
-                    I'm a full stack web developer from Cape Cod, Massachusetts. I believe technology is one
-                    of the best ways to create a better world and I'm fascinated with leveraging powerful
-                    technologies and development techniques to create disruptive and impactful software.
+                Developer with a passion for leveraging powerful technologies to change the world.
+                Recently earned a certificate in Full Stack Development from the University of New Hampshire.
+                Some of the technologies I learned include HTML, CSS, JavaScript, jQuery, node, express, SQL, Mongo and react.js.
+                I also worked with a team in a leadership role to ensure larger projects got done in a timely manner.
+                I’m skilled at combining a strong technical understanding with a good sense of design to create powerful and intuitive web apps.
+                I enjoy self-directed learning and plan to continue learning new technologies in my spare time.
+                In particular, I’m excited to learn Python and explore AI and machine learning.
+                Ideally, I’d like to use my skills to help create environmentally sustainable companies.
+                I hope to bring value to a talented and diverse team with my creative problem-solving skills and my ability to learn quickly.
                 </p>
             </section>
         </div>
