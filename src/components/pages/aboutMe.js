c7292296f265fe420ce6e8be81611cf73ca2c8a6
Dylan Cole <logboarddjc@gmail.com>
2021-09-14 13:36:46 -0400

    pull in changes from main

--- src/components/pages/aboutMe.js
+++ src/components/pages/aboutMe.js
@@ -6,14 +6,14 @@ export default function AboutMe() {
             <section id="about-me" className="default-tag-structure">
                 <h1>About Me</h1>
                 <p className="article-text">
-                Developer with a passion for leveraging powerful technologies to change the world.
-                Recently earned a certificate in Full Stack Development from the University of New Hampshire.
-                Some of the technologies I learned include HTML, CSS, JavaScript, jQuery, node, express, SQL, Mongo and react.js.
-                I also worked with a team in a leadership role to ensure larger projects got done in a timely manner.
-                I’m skilled at combining a strong technical understanding with a good sense of design to create powerful and intuitive web apps.
-                I enjoy self-directed learning and plan to continue learning new technologies in my spare time.
-                In particular, I’m excited to learn Python and explore AI and machine learning.
-                Ideally, I’d like to use my skills to help create environmentally sustainable companies.
+                Developer with a passion for leveraging powerful technologies to change the world. 
+                Recently earned a certificate in Full Stack Development from the University of New Hampshire. 
+                Some of the technologies I learned include HTML, CSS, JavaScript, jQuery, node, express, SQL, Mongo and react.js. 
+                I also worked with a team in a leadership role to ensure larger projects got done in a timely manner. 
+                I’m skilled at combining a strong technical understanding with a good sense of design to create powerful and intuitive web apps. 
+                I enjoy self-directed learning and plan to continue learning new technologies in my spare time. 
+                In particular, I’m excited to learn Python and explore AI and machine learning. 
+                Ideally, I’d like to use my skills to help create environmentally sustainable companies. 
                 I hope to bring value to a talented and diverse team with my creative problem-solving skills and my ability to learn quickly.
                 </p>
             </section>
