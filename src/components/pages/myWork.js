d431355d5a9542ca10395eca326dd2e306d989a8
Dylan Cole <logboarddjc@gmail.com>
2021-09-14 14:59:22 -0400

    add description and list of technologies used for each project

--- src/components/pages/myWork.js
+++ src/components/pages/myWork.js
@@ -13,8 +13,16 @@ const projects = [
         image: swosaat,
         name: 'SWOSAAT',
         github: 'https://github.com/Deucedadorian/Subscription-Tracker',
-        description: "",
-        technologies: "",
+        description: `SWOOSAT is a full-stack application designed to
+        track recurring online subscription spending using analytics to 
+        track those expenses.`,
+        technologies: [
+            'Handlebars.js',
+            'Node.js',
+            'Express.js',
+            'MySQL Database',
+            'Chart.js'
+        ],
         key: 1,
     },
     {
@@ -22,8 +30,15 @@ const projects = [
         image: reciflixor,
         name: 'Reciflixor',
         github: 'https://github.com/UNH-Bootcamp-Projects/project-1-group-a',
-        description: "",
-        technologies: "",
+        description: `Reciflixor is an application where movie lovers can select
+        a movie and enjoy a recipe from that movie to experience the world the directors created.
+        Reciflixor is a movie immersion experience!`,
+        technologies: [
+            'Foundations CSS Framework',
+            'JavaScript',
+            'The Movie Database API',
+            'YouTube API'
+        ],
         key: 2,
 
     },
@@ -32,8 +47,15 @@ const projects = [
         image: blogSite,
         name: 'CMS-style blog site',
         github: 'https://github.com/Deucedadorian/14-Model-View-Controller-MVC-Tech-Blog',
-        description: "",
-        technologies: "",
+        description: `A CMS-style blog site similar to a Wordpress site,
+        where developers can publish their blog posts and comment on other developers’ posts as well.
+        It follows the MVC paradigm in its architectural structure.`,
+        technologies: [
+            'Handlebars.js',
+            'Sequelize',
+            'express-session',
+            'JavaScript'
+        ],
         key: 3,
     },
     {
@@ -41,8 +63,16 @@ const projects = [
         image: weatherApp,
         name: 'Weather Dashboard',
         github: 'https://github.com/Deucedadorian/06-Server-Side-APIs-Weather-Dashboard',
-        description: "",
-        technologies: "",
+        description: `This app allows you to search for current and future weather conditions by city name.
+        Once a city has been searched, the city is saved to local storage and I button is appended to the search section.`,
+        technologies: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'Bootstrap',
+            'Day.js',
+            'zepto.js'
+        ],
         key: 4,
     },
     {
@@ -50,8 +80,17 @@ const projects = [
         image: scheduler,
         name: 'Work Day scheduler',
         github: 'https://github.com/Deucedadorian/05-Third-Party-APIs-Work-Day-Scheduler',
-        description: "",
-        technologies: "",
+        description: `This application uses jquery, bootstrap, and moment.js to allow you to plan 
+        out your work day.You can write out your schedule for each hour and the app will help you 
+        keep track of time by coloring each hour with colors for past, present, and future.`,
+        technologies: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'Bootstrap',
+            'jQuery',
+            'moment.js',
+        ],
         key: 5,
     },
     {
@@ -59,8 +98,14 @@ const projects = [
         image: NoteTaker,
         name: 'Code Quiz',
         github: 'https://github.com/Deucedadorian/11-Express.js-Note-Taker',
-        description: "",
-        technologies: "",
+        description: `This application is used to write and save notes. It uses 
+        an Express.js back end and saves and retrieves note data from a JSON file.`,
+        technologies: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'express',
+        ],
         key: 6,
     },
 ];
