02e67226b4a7784b8f27d4876c60bde11866a7b9
Dylan Cole <logboarddjc@gmail.com>
2021-09-16 14:46:49 -0400

    format description of projects

--- src/components/pages/project.js
+++ src/components/pages/project.js
@@ -5,21 +5,14 @@ export default function Card(props) {
         <div className="project-card">
             <a className="image-container" href={props.deploy} target="_blank" rel="noreferrer">
                 <img className="project-images" src={props.image} alt="App Banner" />
-                {/* TODO: fix overlay to show on hover */}
-                <div className="overlay">
-                    <p>Technologies</p>
-                    <ul>
-                        <li>test</li>
-                        <li>test2</li>
-                        <li>test3</li>
-                    </ul>
-                </div>
             </a>
             <section className="project-info">
-                <h2 className="project-title">{props.name}</h2>
-                <a href={props.github} target="_blank" rel="noreferrer">
-                    <h3>Github</h3>
-                </a>
+                <div className="title-section">
+                    <h2 className="project-title">{props.name}</h2>
+                    <a href={props.github} target="_blank" rel="noreferrer">
+                        <h3>Github</h3>
+                    </a>
+                </div>
                 <p>{props.description}</p>
             </section>
         </div>
