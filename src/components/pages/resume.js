30974cb25ca883d7f6d7ff3adb91049b5def14ca
Dylan Cole <logboarddjc@gmail.com>
2021-09-17 15:54:23 -0400

    update some resume items and styling

--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -4,8 +4,12 @@ import '../../assets/css/contact.css';
 
 export default function Resume() {
     return (
-        <ul className='default-tag-structure resume-list'>
-            <li><h3>Languages</h3></li>
+        <section className="resume-section">
+            <a href="https://docs.google.com/document/d/1OIVoojwJsLW0Y977_YJDl1uth4zm0cpItfo19QxTWNI/edit?usp=sharing" target="_blank" rel="noreferrer">
+                <button className="link-list form" id="dl-btn" >Download Resume</button>
+            </a>  
+            <ul className='default-tag-structure resume-list'>
+                <li><h3>Languages</h3></li>
                 <li>HTML</li>
                 <li>CSS</li>
                 <li>JavaScript</li>
@@ -13,17 +17,22 @@ export default function Resume() {
                 <li>Java</li>
                 <li>Processing</li>
                 <li>Python</li>
-                <li>Mysql</li>
                 <li><h3>Tools</h3></li>
                 <li>jQuery</li>
                 <li>Bootstrap</li>
                 <li>Node</li>
                 <li>handlebars</li>
                 <li>Command Line</li>
+                <li>Mysql</li>
+                <li>MongoDB</li>
+                <li>Express</li>
+                <li>React</li>
+                <li></li>
+                <li></li>
                 <li><h3>Programs</h3></li>
                 <li>Arduino Development Environment</li>
                 <li>Blender</li>
-                <li>DiVinci Resolv</li>
+                <li>DiVinci Resolve</li>
                 <li>Eclipse</li>
                 <li>Final Cut Pro X</li>
                 <li>GIMP</li>
@@ -34,9 +43,7 @@ export default function Resume() {
                 <li>VNC</li>
                 <li>VS code</li>
                 <li>Oracle VirtualBox</li>
-            <a href="https://docs.google.com/document/d/1OIVoojwJsLW0Y977_YJDl1uth4zm0cpItfo19QxTWNI/edit?usp=sharing" target="_blank" rel="noreferrer">
-            <button className="link-list form" id="dl-btn" >Download Resume</button>
-            </a>
-        </ul>
+            </ul>
+        </section>
     )
 }
