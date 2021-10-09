a2531b06c181eca4eaba49f2e259df54f89c790b
Dylan Cole <logboarddjc@gmail.com>
2021-10-09 17:05:12 -0400

    refactor navtabs lil bit

--- Update_Changes.md
+++ Update_Changes.md
@@ -1,7 +1,3 @@
 To deploy a new version when I update changes, I need to run:
 
-```npm install --save gh-pages```
-
-Then, I need to run:
-
 ```npm run deploy```
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -3,38 +3,33 @@ import React from 'react';
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
-function NavTabs({ currentPage, handlePageChange }) {
+function NavTabs(props) {
   return (
     <div className="default-tag-structure" id="nav-section">
       <h1 id="title">Dylan Cole Portfolio</h1>
       <nav className="nav nav-tabs link-list">
         <a
-          href="#about" onClick={() => handlePageChange('about')}
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'about' ? 'nav-link active' : 'nav-link'}
+          href="#about" onClick={() => props.handlePageChange('about')}
+          className={props.currentPage === 'about' ? 'nav-link active' : 'nav-link'}
         >
           About
         </a>
         <a
-          href="#my-work" onClick={() => handlePageChange('my-work')}
-          // Check to see if the currentPage is `MyWork`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'my-work' ? 'nav-link active' : 'nav-link'}
+          href="#my-work" onClick={() => props.handlePageChange('my-work')}
+          className={props.currentPage === 'my-work' ? 'nav-link active' : 'nav-link'}
         >
           My Work
         </a>
         <a
-          href="#contact" onClick={() => handlePageChange('contact')}
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'contact' ? 'nav-link active' : 'nav-link'}
+          href="#contact" onClick={() => props.handlePageChange('contact')}
+          className={props.currentPage === 'contact' ? 'nav-link active' : 'nav-link'}
         >
           Contact
         </a>
         <a
           href="#resume"
-          onClick={() => handlePageChange('resume')}
-          // This is a conditional (ternary) operator that checks to see if the current page is "Resume"
-          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === 'resume' ? 'nav-link active' : 'nav-link'}
+          onClick={() => props.handlePageChange('resume')}
+          className={props.currentPage === 'resume' ? 'nav-link active' : 'nav-link'}
         >
           Resume
         </a>
